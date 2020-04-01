a12e78346a35e8d3357dc811c164d8ce515030ae
Pablo Rosas <pablojose.rosas.next@bbva.com>
2020-04-01 13:45:24 +0200

    Primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,13 @@
+//Guardar en el caché dinámico
+function actualizaCacheDinamico(dynamicCache, req, res) {
+    
+    if(res.ok){
+        return caches.open(dynamicCache).then(cache => {
+            cache.put(req, res.clone());
+
+            return res.clone();
+        });
+    } else {
+        return res;
+    }
+}
